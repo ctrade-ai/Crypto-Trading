2ab8c9a3edb3931ad778be748b7beb9e543fceb7
Avi Takiyar <avi.takiyar@gmail.com>
2024-08-14 22:17:01 +0530

    Production based config

--- config/config.js
+++
@@ -1,8 +0,0 @@
-const env = require("./env");
-
-const args = process.argv.slice(2);
-const [envType = 'stage'] = args; // Default to 'stage' if no argument is provided
-
-const config = env[envType];
-
-module.exports = config;
