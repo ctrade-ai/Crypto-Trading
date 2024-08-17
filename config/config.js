ef03215fdbdeed7486eba07391793a9bfa60f9c2
Avi Takiyar <avi.takiyar@gmail.com>
2024-08-17 08:58:42 +0530

    config changed

--- config/config.js
+++ config/config.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 
 const config = {
-    BASE_URL: process.env.NODE_ENV === "prod" ? process.env.BASE_URL_PROD : process.env.BASE_URL_STAGE,
+    BASE_URL: process.env.NODE_ENV === "prod" ? "https://api.binance.com" : "https://testnet.binance.vision",
     API_KEY: process.env.NODE_ENV === "prod" ? process.env.API_KEY_PROD : process.env.API_KEY_STAGE,
     API_SECRET: process.env.NODE_ENV === "prod" ? process.env.API_SECRET_PROD : process.env.API_SECRET_STAGE,
     MARKET_PRICES_PATH: "/api/v3/ticker/price",
